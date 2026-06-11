943d258494dff1fc6ee4f999587127695b28a81a
silnith <silnith@gmail.com>
2026-06-10 20:16:35 -0500

    Make the various "get bits" internal visibility.

--- libraries/FloatUtils/InexactFloatComparer.cs
+++ libraries/FloatUtils/InexactFloatComparer.cs
@@ -340,7 +340,7 @@ namespace Silnith.FloatUtils
         /// </summary>
         /// <param name="bits">The float bits to parse.</param>
         /// <returns>A value in the range <c>[0, 1]</c>.  <c>0</c> represents positive, <c>1</c> represents negative.</returns>
-        public uint GetSignBit(uint bits)
+        internal uint GetSignBit(uint bits)
         {
             return (bits >> 31) & 1u;
         }
@@ -351,7 +351,7 @@ namespace Silnith.FloatUtils
         /// </summary>
         /// <param name="bits">The float bits to parse.</param>
         /// <returns>A value in the range <c>[0, 255]</c>.</returns>
-        public uint GetExponentBits(uint bits)
+        internal uint GetExponentBits(uint bits)
         {
             return (bits >> 23) & 0xffu;
         }
@@ -362,7 +362,7 @@ namespace Silnith.FloatUtils
         /// </summary>
         /// <param name="bits">The float bits to parse.</param>
         /// <returns>A 23-bit number in the range <c>[0, 0x7fffff]</c>.</returns>
-        public uint GetMantissaBits(uint bits)
+        internal uint GetMantissaBits(uint bits)
         {
             return bits & 0x7f_ffffu;
         }
